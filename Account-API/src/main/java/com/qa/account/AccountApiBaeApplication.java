337a4277402b167a849c9712e1d0f96a0c7383c4
jordan <jharrison1@qa.com>
2021-04-28 13:07:07 +0100

    added base api

---
+++ Account-API/src/main/java/com/qa/account/AccountApiBaeApplication.java
@@ -0,0 +1,34 @@
+package com.qa.account;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+
+@SpringBootApplication
+public class AccountApiBaeApplication {
+
+	public static void main(String[] args) throws JsonProcessingException {
+		ApplicationContext beanBag = SpringApplication.run(AccountApiBaeApplication.class, args);
+
+		/*
+		 * // EXAMPLE FOR MICHAEL - DO NOT USE IN PRODUCTION!!111!!
+		 * 
+		 * ObjectMapper mapper = beanBag.getBean(ObjectMapper.class); AccountController
+		 * controller = beanBag.getBean(AccountController.class);
+		 * 
+		 * // incoming from postman or w/e String newAccountAsJSON =
+		 * "{\"username\":\"Mike\",\"password\":\"mickeyD\"}";
+		 * 
+		 * Account account = mapper.readValue(newAccountAsJSON, Account.class);
+		 * 
+		 * Account savedAccount = controller.createAccount(account).getBody();
+		 * 
+		 * // outgoing String savedJSON = mapper.writeValueAsString(savedAccount);
+		 * 
+		 * System.out.println("OUTPUT: " + savedJSON);
+		 */
+	}
+
+}
---
+++ Account-API/src/test/java/com/qa/account/service/AccountServiceUnitTest.java
@@ -0,0 +1,54 @@
+package com.qa.account.service;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import com.qa.account.persistence.domain.Account;
+import com.qa.account.persistence.repo.AccountRepo;
+
+@SpringBootTest
+public class AccountServiceUnitTest {
+
+	@Autowired
+	private AccountService service;
+
+	@MockBean
+	private NumberGenService numberGenerator;
+
+	@MockBean
+	private PrizeGenService prizeGenerator;
+
+	@MockBean
+	private AccountRepo repo;
+
+	@Test
+	void testRegister() {
+		// GIVEN
+
+		final Account testAccount = new Account("jh", "pass");
+
+		final String testAccountNumber = "c1234567890";
+
+		final int testPrize = 10_000;
+
+		final Account testRegisteredAccount = new Account(1L, testAccount.getUsername(), testAccount.getPassword(),
+				testAccountNumber, testPrize);
+
+		// WHEN
+
+		Mockito.when(this.numberGenerator.genNumber()).thenReturn(testAccountNumber);
+
+		Mockito.when(this.prizeGenerator.genPrize(testAccountNumber)).thenReturn(testPrize);
+
+		Mockito.when(this.repo.save(testAccount)).thenReturn(testRegisteredAccount);
+
+		// THEN
+
+		assertThat(this.service.register(testAccount)).isEqualTo(testRegisteredAccount);
+	}
+}
